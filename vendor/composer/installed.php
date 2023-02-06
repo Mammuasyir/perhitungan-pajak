54e7765202703e69aa15602287fe563f973048a6
Humam Muasyir <mammuasyir0101@gmail.com>
2023-02-06 11:36:46 +0800

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'modernoop/pph21',
+        'reference' => '87e9dc0502b0ac3ef11a637c92ab512d9fd1b8ca',
+        'name' => 'oop/pph21',
         'dev' => true,
     ),
     'versions' => array(
-        'modernoop/pph21' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'oop/pph21' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '87e9dc0502b0ac3ef11a637c92ab512d9fd1b8ca',
             'dev_requirement' => false,
         ),
     ),
